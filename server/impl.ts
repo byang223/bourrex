4788e2d53d10d2567ba43c3ca289661efbfe275e
byang <byang223@gmail.com>
2022-07-04 17:09:53 -0700

    server: basic game initializations for bourre

--- server/impl.ts
+++ server/impl.ts
@@ -1,99 +1,88 @@
 import { Methods, Context } from "./.hathora/methods";
 import { Response } from "../api/base";
 import {
-  UserId,
+  Configuration,
+  GameState,
+  Phase,
+  Trick,
+  Player,
+  Game,
   PlayerState,
+  UserId,
   IInitializeRequest,
-  IFireCannonRequest,
-  ISetOrientationRequest,
+  IConfigureGameRequest,
   IJoinGameRequest,
+  IStartGameRequest,
+  IStopGameRequest,
+  IFoldHandRequest,
+  ISwapCardsRequest,
+  IPlayRequest,
 } from "../api/types";
-import { InternalShip } from "./Ship";
-import { InternalCannonBall } from "./CannonBall";
+import { Card, Cards, createDeck, drawCardsFromDeck, findHighestHands } from "@pairjacks/poker-cards";
 
 type InternalState = {
-  ships: InternalShip[];
-  cannonBalls: InternalCannonBall[];
-};
+  game: Game,
+  deck: Cards;
+}
 
-const MAP_WIDTH = 1200;
-const MAP_HEIGHT = 900;
+const MAX_PLAYERS = 7;
 
 export class Impl implements Methods<InternalState> {
-  initialize(ctx: Context, args: IInitializeRequest): InternalState {
-    return { ships: [], cannonBalls: [] };
+  initialize(ctx: Context, request: IInitializeRequest): InternalState {
+    return {
+      game: {
+        configuration: {
+          timeLimit: 20,
+          ante: 1,
+        },
+        state: GameState.STOPPED,
+        round: 0,
+        phase: Phase.FOLD,
+        players: [],
+        pot: 0,
+      },
+      deck: [],
+    };
+  }
+  configureGame(state: InternalState, userId: UserId, ctx: Context, request: IConfigureGameRequest): Response {
+    state.game.configuration.timeLimit = request.timeLimit;
+    state.game.configuration.ante = request.ante;
+    return Response.ok();
   }
   joinGame(state: InternalState, userId: UserId, ctx: Context, request: IJoinGameRequest): Response {
-    if (state.ships.find((s) => s.player === userId) !== undefined) {
-      return Response.error("Already joined");
+    if (state.game.players.length >= 7) {
+      return Response.error("no more than 7 players allowed");
     }
-    if (state.ships.length >= 6) {
-      return Response.error("Room is full");
-    }
-    state.ships.push(createShip(userId, ctx));
+    state.game.players.push({
+      id: userId,
+      name: request.name,
+      handSize: 0,
+      chips: request.chips,
+      seatPosition: state.game.players.length,
+      tricks: [],
+    })
     return Response.ok();
   }
-  setOrientation(state: InternalState, userId: UserId, ctx: Context, request: ISetOrientationRequest): Response {
-    const ship = state.ships.find((s) => s.player === userId);
-    if (ship === undefined) {
-      return Response.error("Not joined game");
-    }
-    if (!ship.setOrientation(request.orientation, request.accelerating)) {
-      return Response.error("Invalid action");
-    }
-    return Response.ok();
+  startGame(state: InternalState, userId: UserId, ctx: Context, request: IStartGameRequest): Response {
+    return Response.error("Not implemented");
   }
-  fireCannon(state: InternalState, userId: UserId, ctx: Context, request: IFireCannonRequest): Response {
-    const ship = state.ships.find((s) => s.player === userId);
-    if (ship === undefined) {
-      return Response.error("Not joined game");
-    }
-    if (!ship.fire(ctx.time)) {
-      return Response.error("Invalid action");
-    }
-    state.cannonBalls.push(createCannonBall(ctx.chance.integer({ min: 0, max: 1e6 }), ship, Math.PI / 2));
-    state.cannonBalls.push(createCannonBall(ctx.chance.integer({ min: 0, max: 1e6 }), ship, -Math.PI / 2));
-    return Response.ok();
+  stopGame(state: InternalState, userId: UserId, ctx: Context, request: IStopGameRequest): Response {
+    return Response.error("Not implemented");
+  }
+  foldHand(state: InternalState, userId: UserId, ctx: Context, request: IFoldHandRequest): Response {
+    return Response.error("Not implemented");
+  }
+  swapCards(state: InternalState, userId: UserId, ctx: Context, request: ISwapCardsRequest): Response {
+    return Response.error("Not implemented");
+  }
+  play(state: InternalState, userId: UserId, ctx: Context, request: IPlayRequest): Response {
+    return Response.error("Not implemented");
   }
   getUserState(state: InternalState, userId: UserId): PlayerState {
     return {
-      ships: state.ships.map((ship) => ship.toPlayerState()),
-      cannonBalls: state.cannonBalls.map((cannonBall) => cannonBall.toPlayerState()),
+      game: state.game,
+      hand: [],
     };
   }
-  onTick(state: InternalState, ctx: Context, timeDelta: number): void {
-    state.ships.forEach((ship) => {
-      ship.update(timeDelta);
-      if (ship.body.x < 0 || ship.body.y < 0 || ship.body.x >= MAP_WIDTH || ship.body.y >= MAP_HEIGHT) {
-        ship.die();
-      }
-    });
-    for (let i = state.cannonBalls.length - 1; i >= 0; i--) {
-      state.cannonBalls[i].update(timeDelta);
-      const { x, y } = state.cannonBalls[i].body;
-      if (x < 0 || y < 0 || x >= MAP_WIDTH || y >= MAP_HEIGHT) {
-        state.cannonBalls.splice(i, 1);
-      }
-    }
-    state.ships.forEach((ship) => {
-      state.cannonBalls.forEach((cannonBall, idx) => {
-        if (ship.player !== cannonBall.firedBy && ship.body.collides(cannonBall.body)) {
-          ship.handleCollision();
-          state.cannonBalls.splice(idx, 1);
-        }
-      });
-    });
-  }
-}
-
-function createShip(player: UserId, ctx: Context) {
-  return new InternalShip(
-    player,
-    ctx.chance.integer({ min: 0, max: MAP_WIDTH }),
-    ctx.chance.integer({ min: 0, max: MAP_HEIGHT })
-  );
-}
-
-function createCannonBall(id: number, ship: InternalShip, dAngle: number) {
-  return new InternalCannonBall(id, ship.player, ship.body.x, ship.body.y, ship.body.angle + dAngle);
+  onTick(state: InternalState, ctx: Context, timeDelta: number): void {}
 }
